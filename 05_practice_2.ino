882c639c6faeea92dd07d12a115b56dd00473f18
Seungho-Min <84025029+Seungho-Min@users.noreply.github.com>
2021-09-18 14:14:53 +0900

    Create 05_practice_2.ino

---
+++ 05_practice_2.ino
@@ -0,0 +1,19 @@
+#define PIN_LED 7
+
+void setup() {
+  pinMode(7, OUTPUT);
+  digitalWrite(7, LOW);
+  delay(1000);
+}
+
+void loop() {
+ for (int i =0; i < 5; i++) {
+  digitalWrite(7, HIGH);
+  delay(100);
+  digitalWrite(7, LOW);
+  delay(100);
+ }
+  while(1) {
+    digitalWrite(7, HIGH);
+  }
+}
